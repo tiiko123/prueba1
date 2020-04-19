c65a3321206d1586b10fe684b0d136b864d1f5a6
Francisco <1830275@upv.edu.mx>
2020-04-19 15:47:11 -0500

    Agregamos primer commit

---
+++ src/main/java/com/prueba1/App.java
@@ -0,0 +1,14 @@
+package com.prueba1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Si jala");
+    }
+}
